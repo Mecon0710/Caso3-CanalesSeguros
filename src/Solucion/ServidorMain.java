d520a089771fd1c396bdca5547616ba091c583dd
Mecon0710 <m.contrerasr@uniandes.edu.co>
2022-11-05 21:16:59 -0500

    Arreglos

--- src/Solucion/ServidorMain.java
+++ src/Solucion/ServidorMain.java
@@ -1,4 +1,4 @@
-package seguridad20222_servidor;
+package Solucion;
 
 import java.io.IOException;
 import java.net.ServerSocket;
--- src/Solucion/SrvThread.java
+++ src/Solucion/SrvThread.java
@@ -1,4 +1,4 @@
-package seguridad20222_servidor;
+package Solucion;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
